2f3bd3ab3299c6d395e7ad4edf75c5915a20e7f7
ketansoni <ketansony@gmail.com>
2014-12-13 16:46:20 +0530

    route correction

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Feedbackapp::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'feedbacks#new'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
